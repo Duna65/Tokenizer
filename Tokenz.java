d5601927caa9330dcfc4601b2119683c225fabbe
Dallin Lyman <142834133+Duna65@users.noreply.github.com>
2025-02-25 14:37:29 -0700

    Add files via upload

--- Tokenz.java
+++ Tokenz.java
@@ -8,10 +8,11 @@ import java.util.StringTokenizer;
 
 public class Tokenz {
     public static void main(String[] args) {
-        float cit=0;//variables for totals, cit is the total frequency added up
-        float cit0=0;//cit0 is the amount of frequency variables
-        float cit1=0;//cit1 is the average of the frequencies
-        float cit01=0;//cit01 is the total of the percents
+        float cit = 0;//variables for totals, cit is the total frequency added up
+        float cit0 = 0;//cit0 is the amount of frequency variables
+        float cit1 = 0;//cit1 is the average of the frequencies
+        float cit01 = 0;//cit01 is the total of the percents
+        boolean newline = false;//boolean to make the line jump to the next line
          try {
       File File0 = new File("letter_frequency.csv");//new file read
       Scanner Read = new Scanner(File0);//new scanner
@@ -19,6 +20,7 @@ public class Tokenz {
         String data = Read.nextLine();
         StringTokenizer st = new StringTokenizer(data," ");//separates the data 
       while (st.hasMoreTokens()) {//reads the tokens and prints them
+            newline = false;
             String tokn = st.nextToken().replace(",","");//makes the next token a variable, and removes commas
             if(false==tokn.contains("\"")){//tests if the words have quotes
             float ci = Float.parseFloat(tokn);//changes the variable into a float
@@ -28,8 +30,13 @@ public class Tokenz {
             }
             if(ci<=100){//adds all of the percents together
                 cit01+=ci;
+                newline = true;
             }}
-            System.out.println(tokn);//prints the variable
+            if (newline==true){//prints the variable. If the variable is a percent then it prints a new line 
+                System.out.println(tokn);
+            } else {
+                System.out.print(tokn+"    ");
+            }
             }
         }
       Read.close();
